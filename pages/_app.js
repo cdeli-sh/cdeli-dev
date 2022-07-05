3a53917ad3af35d13a2067ad56ad6b237814c3ca
cdeli <celien.aupiais@molecul.fr>
2022-07-06 01:37:00 +0200

    (feat) : analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
 
+import PlausibleProvider from 'next-plausible'
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <PlausibleProvider domain="cdeli.dev" customDomain='https://analytics.molecul.fr' trackLocalhost={true} selfHosted={true} enabled={true}>
+      <Component {...pageProps} />
+    </PlausibleProvider>
+  )
 }
 
 export default MyApp
